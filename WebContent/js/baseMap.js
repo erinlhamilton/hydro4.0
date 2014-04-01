91718c62160abf6efb042f272780afd66a26db6c
Erin Hamilton <erin@erinhamilton.me>
2014-04-01 15:00:45 -0500

    Slight update to css, addition of empty JAVA classes

--- WebContent/js/baseMap.js
+++ WebContent/js/baseMap.js
@@ -1,3 +1,10 @@
 /**
  * 
  */
+
+var map = L.map('map').setView([44.500, -89.4000], 8);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    maxZoom: 18
+}).addTo(map);
