d52ab3e372f51fa7c3a6bea13abdd1da1c928dc1
Erin Hamilton <erin@erinhamilton.me>
2014-04-14 13:31:08 -0500

    Addition of Green Bay inset map

--- WebContent/js/baseMap.js
+++ WebContent/js/baseMap.js
@@ -2,9 +2,27 @@
  * 
  */
 
-var map = L.map('map').setView([44.500, -89.4000], 8);
+var fwMap = L.map('fwMap').setView([44.500, -88.7000], 8);
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18
-}).addTo(map);
+}).addTo(fwMap);
+
+var gbMap = L.map('gbMap').setView([44.700, -87.800], 10);
+
+L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    maxZoom: 18
+}).addTo(gbMap);
+
+
+
+//WMS request for Michigan tech remote sensing imagery of Green Bay
+//var mtri = L.tileLayer.wms("http://geoserver2.mtri.org/geoserver/WaterRemoteSensing/wms?", {
+//    layers: 'WaterRemoteSensing:SM_Michigan_20130620',
+//    format: 'image/png',
+//    version: '1.1.0',
+//    attribution: 'Michigan Tech',
+//    srs:'EPSG:4326'
+//}).addTo(map);
