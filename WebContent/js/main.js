73dc6951e8369201cfbae83462ad6befc8c1bef8
Erin Hamilton <erin@erinhamilton.me>
2014-04-17 13:05:17 -0500

    Map height adjusts based on window height. Including Foundation and simple-slider libraries.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,3 +1,10 @@
 /**
  * 
  */
+
+
+
+/*Set the height of the map to half the size of the current window*/
+$(window).on("resize", function() {
+    $("#map").height($(window).height()/2);
+}).trigger("resize");
